f11779db4db94c4294732aab0934c65af80bbaf7
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-06 15:22:34 +0200

    stupid spaces commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
-{
-
+{  
     public function index()
     {
         return view('welcome');
