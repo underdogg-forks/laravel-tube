310574c4561db3da0fb70aaa08dfb23083a54910
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-06 14:01:21 +0200

    showing progress bar with likes and dislikes inside show view

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -4,10 +4,15 @@
 @section('content')
 
     <h1 class='display-2'> {{ $data['video']->title }} </h1>
-    
-    <video width="600" height="400" controls>
+        
+    <video width="80%" controls>
         <source src="/storage/videos/{{ $data['video']->name }}">
     </video>
+
+    <div class="progress" style="margin-left:10%; width:80%;" >
+        <div class="progress-bar bg-success" role="progressbar" style="width:{{$data['positiveRates']}}%" 
+        aria-valuenow="{{$data['positiveRates']}}" aria-valuemin="0" aria-valuemax="100"></div>     
+    </div>
 
     <div class='social-buttons'>
         <a class='btn btn-success btn-md' href='/like/{{$data['video']->id}}'>Like </a>
