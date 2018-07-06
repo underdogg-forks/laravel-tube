ac88e7b9f93a28a9d2592bcc9697049bb0a8e560
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-06 13:37:21 +0200

    SocialController enables logged in user to either like or dislike given video

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Rate;
+use App\Models\Video;
 
 class SocialController extends Controller
 {
@@ -11,10 +13,30 @@ class SocialController extends Controller
     }
 
     public function like($video_id){
-        return 'like';
+        
+        if(!Video::find($video_id))
+            return redirect('/')->with('danger','Video not found');
+
+        Rate::create([
+            'user_id' => auth()->user()->id,
+            'video_id' => $video_id,
+            'type' => 'L'
+        ]);
+
+        return redirect('/video/'.$video_id)->with('success','You just liked this video !');
     }
 
     public function dislike($video_id){
-        return 'dislike';
+        
+        if(!Video::find($video_id))
+            return redirect('/')->with('danger','Video not found');
+
+        Rate::create([
+            'user_id' => auth()->user()->id,
+            'video_id' => $video_id,
+            'type' => 'D'
+        ]);
+
+        return redirect('/video/'.$video_id)->with('success','You just disliked this video !');
     }
 }
