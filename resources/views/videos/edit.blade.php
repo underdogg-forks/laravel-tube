b489d133d7b36f61af9f10eb21daf8f247b384e1
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-07 19:21:00 +0200

    view for editing posts

---
+++ resources/views/videos/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title') Edit your video @endsection
+
+@section('content')
+    
+    <div style='margin-top:50px;'></div>
+
+   {!! Form::open(['action' => ['VideoController@edit'], 'method'=> 'post',
+      'class' => 'form-control', 'style' => 'border:none;'
+        ]) !!}
+                                                
+        <div class="form-group">
+            {{ Form::text('title',$video->title,['class' => 'form-control','placeholder' => 'title',
+                'required'
+            ]) }}    
+        </div>
+
+        <div class="form-group">
+          {{ Form::textarea('description',$video->description,['class' => 'form-control','placeholder' => 'description'
+          , 'required'
+          ]) }}  
+        </div>    
+        {{ Form::hidden('video_id',$video->id) }}                                            
+        {{ Form::submit('Save changes',['class' => 'btn btn-success']) }}
+        {!! Form::close() !!}
+
+@endsection
