c1bf924cdde309baf2c8bafd4698af321d18794f
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-07 19:20:15 +0200

    AccountController->edit return view for editing videos

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Video;
+use App\Models\Rate;
+use Illuminate\Support\Facades\Storage;
 
 class AccountController extends Controller
 {
@@ -17,10 +19,30 @@ class AccountController extends Controller
     }
 
     public function edit($video_id){
-        //
+        $video = Video::where([
+            'id' => $video_id,
+            'user_id' => auth()->user()->id    
+        ])->first();
+        
+        if(!$video)
+            return redirect('/account')->with('danger','Video not found');
+
+        return view('videos.edit')->with('video',$video);
     }
 
     public function delete($video_id){
-        //
+        
+        $video = Video::where([
+            'user_id' => auth()->user()->id,
+            'id' => $video_id
+        ])->first();
+        
+        if(!$video)
+            return redirect('/account')->with('danger','Video not found');
+
+        Storage::delete('public/videos/'.$video->name);
+        $rates = Rate::where('video_id','=',$video_id)->delete();
+        $video->delete();
+        return redirect('/account')->with('success','Video deleted');
     }
 }
