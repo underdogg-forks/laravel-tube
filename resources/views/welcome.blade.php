9254a5990f6262a2f057124b4589f2fdcb6b5f38
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-08 16:28:41 +0200

    main view has navigation buttons inside bootstrap jumbatron

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,12 @@
 @section('content')
 
     @auth
-        <a  href="/upload" class="btn btn-primary btn-lg"> Upload your video </a>
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome!</h1>
+            <a class="btn btn-primary btn-lg" href="/account" style="margin-right:15px;"> 
+            Dashboard </a>
+            <a class="btn btn-success btn-lg" href="/upload"> Upload </a>
+        </div>
     @endauth
 
 @endsection
