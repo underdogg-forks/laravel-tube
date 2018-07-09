e2fd475f2a62556d80e15f4b0bb443389341be78
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-09 15:08:33 +0200

    RatesController uses Repositories instead of Models

--- app/Http/Controllers/Social/RatesController.php
+++ app/Http/Controllers/Social/RatesController.php
@@ -4,80 +4,56 @@ namespace App\Http\Controllers\Social;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Rate;
-use App\Models\Video;
+
+use App\Repositories\VideoRepository;
+use App\Repositories\RateRepository;
 
 class RatesController extends Controller
-{
-    public function __construct(){
+{   
+    private $videoRepo;
+    private $rateRepo;
+    
+    public function __construct(VideoRepository $videoRepo, RateRepository $rateRepo){
         $this->middleware('auth');
+        $this->videoRepo = $videoRepo;
+        $this->rateRepo = $rateRepo;
     }
 
     public function like($video_id){
         
-        if(!Video::find($video_id))
+        if(!$this->videoRepo->find($video_id))
             return redirect('/')->with('danger','Video not found');
 
-        if($this->alreadyLiked($video_id)){
-            Rate::where('video_id','=',$video_id)->delete();
+        if($this->rateRepo->alreadyLiked($video_id)){
+            $this->rateRepo->deleteRate($video_id);
             return redirect('/video/'.$video_id)->with('success','You no longer like this video !');
         }
 
-        else if($this->alreadyDisliked($video_id)){
-            Rate::where('video_id','=',$video_id)->delete();
+        else if($this->rateRepo->alreadyDisliked($video_id)){
+            $this->rateRepo->deleteRate($video_id);
         }
         
-        Rate::create([
-            'user_id' => auth()->user()->id,
-            'video_id' => $video_id,
-            'type' => 'L'
-        ]);
+        $this->rateRepo->createAuthLike($video_id);
 
         return redirect('/video/'.$video_id)->with('success','You just liked this video !');
     }
 
     public function dislike($video_id){
         
-        if(!Video::find($video_id))
+        if(!$this->videoRepo->find($video_id))
             return redirect('/')->with('danger','Video not found');
 
-        if($this->alreadyDisliked($video_id)){
-            Rate::where('video_id','=',$video_id)->delete();
+        if($this->rateRepo->alreadyDisliked($video_id)){
+            $this->rateRepo->deleteRate($video_id);
             return redirect('/video/'.$video_id)->with('success','You no longer dislike this video !');
         }
 
-        else if($this->alreadyLiked($video_id)){
-            Rate::where('video_id','=',$video_id)->delete();
+        else if($this->rateRepo->alreadyLiked($video_id)){
+            $this->rateRepo->deleteRate($video_id);
         }
         
-        Rate::create([
-            'user_id' => auth()->user()->id,
-            'video_id' => $video_id,
-            'type' => 'D'
-        ]);
+        $this->rateRepo->createAuthDislike($video_id);
 
         return redirect('/video/'.$video_id)->with('success','You just disliked this video !');
-    }
-
-    private function alreadyLiked($video_id){
-        
-        if(Rate::where([
-            'user_id' => auth()->user()->id,
-            'video_id' => $video_id,
-            'type' => 'L'    
-        ])->get()->count() >0 ) return true;
-
-        else return false;
-    }
-
-    private function alreadyDisliked($video_id){
-        
-        if(Rate::where([
-            'user_id' => auth()->user()->id,
-            'video_id' => $video_id,
-            'type' => 'D'    
-        ])->get()->count() >0 ) return true;
-
-        else return false;
     }
 }
