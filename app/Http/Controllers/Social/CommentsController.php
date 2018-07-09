2412309800aef5c62ea41340a0eaff418dd986c4
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-09 17:21:13 +0200

    Controller for adding comments

--- app/Http/Controllers/Social/CommentsController.php
+++ app/Http/Controllers/Social/CommentsController.php
@@ -5,7 +5,33 @@ namespace App\Http\Controllers\Social;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Repositories\CommentRepository;
+use App\Repositories\VideoRepository;
+
 class CommentsController extends Controller
 {
-    //
+    public function __construct(CommentRepository $commentRepo, VideoRepository $videoRepo){
+        $this->middleware('auth');
+        $this->commentRepo = $commentRepo;
+        $this->videoRepo = $videoRepo;
+    }
+
+    public function store(Request $request){
+        
+        $this->validate($request,[
+            'content' => 'required',
+            'video_id' => 'required'
+        ]);
+        
+        if(!$this->videoRepo->find($request->input('video_id')))
+            return redirect('/')->with('danger','Video not found');
+        
+        $this->commentRepo->create([
+            'video_id' => $request->input('video_id'),
+            'user_id' => auth()->user()->id,
+            'content' => $request->input('content')
+        ]);
+
+        return redirect('/video/'.$request->input('video_id'))->with('success','Comment added');
+    }
 }
