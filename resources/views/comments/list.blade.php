f4f263504bc4bca5512af950bc5c2202ca5aa3d4
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-10 21:55:54 +0200

    comments.list changes

--- resources/views/comments/list.blade.php
+++ resources/views/comments/list.blade.php
@@ -9,8 +9,21 @@
                 
                 <div class='card' style='margin-bottom:15px;'>
                     <div class='card-body'>
+                        
                         <h5 class='card-title'> {{ $comment->creator_name }} </h5>
                         <p class='card-text'>{{ $comment->content }} </p>
+                        
+                        @auth
+                            @if($comment->user_id == auth()->user()->id)
+                                <a class='btn btn-sm btn-primary' href='/comment/edit/{{$comment->id}}'>
+                                    Edit 
+                                </a>
+                                
+                                <a class='btn btn-sm btn-danger' href='/comment/delete/{{$comment->id}}'>
+                                    Delete 
+                                </a>
+                            @endif
+                        @endauth    
                     </div>
                 </div>
 
