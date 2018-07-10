a43e5d8f573e570eb9a6137efa49ef1c0d327432
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-10 21:55:00 +0200

    Routes for editing and deleting videos

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::post('/edit/','Video\VideoController@edit');
 Route::get('like/{video_id}','Social\RatesController@like');
 Route::get('dislike/{video_id}','Social\RatesController@dislike');
 Route::post('/video/','Social\CommentsController@store');
+Route::get('/comment/edit/{id}','Social\CommentsController@edit');
+Route::post('/comment/edit/','Social\CommentsController@update');
+Route::get('/comment/delete/{id}','Social\CommentsController@delete');
 
 Route::get('/account','DashboardController@index');
 Route::get('/edit/{video_id}','DashboardController@edit');
