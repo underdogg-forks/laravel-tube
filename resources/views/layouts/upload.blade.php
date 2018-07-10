dafa66d65803635f2b7945be6a51f397f23adf66
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-11 00:43:14 +0200

    upload has it's own layout wich does not force text-center

---
+++ resources/views/layouts/upload.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/video.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
+</head>
+<body>
+    @include('inc.navbar')
+    
+    <main>
+        <div style="margin-bottom:15px;"></div> 
+        @include('inc.message')
+        @yield('content')               
+    </main>
+</body>
+</html>
