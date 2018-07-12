f7578b663828d4d2d0e3bc4ab309379ef4b2eb83
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-12 17:48:23 +0200

    Routes for likes and dislikes for comments

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,15 @@ Route::get('/video/{id}','Video\VideoController@show');
 Route::get('/delete/{video_id}','Video\VideoController@delete');
 Route::post('/edit/','Video\VideoController@edit');
 
-Route::get('like/{video_id}','Social\RatesController@like');
-Route::get('dislike/{video_id}','Social\RatesController@dislike');
+Route::get('like/{video_id}','Social\RatesController@likeVideo');
+Route::get('dislike/{video_id}','Social\RatesController@dislikeVideo');
 Route::post('/video/','Social\CommentsController@store');
 Route::get('/comment/edit/{id}','Social\CommentsController@edit');
 Route::post('/comment/edit/','Social\CommentsController@update');
 Route::get('/comment/delete/{id}','Social\CommentsController@delete');
+
+Route::get('/comment/like/{id}','Social\RatesController@likeComment');
+Route::get('/comment/dislike/{id}','Social\RatesController@dislikeComment');
 
 Route::get('/account','DashboardController@index');
 Route::get('/edit/{video_id}','DashboardController@edit');
