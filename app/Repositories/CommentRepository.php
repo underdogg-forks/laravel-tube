1bbd3262b6573aeb30482fd5c3b0e15edd74869e
LeonCzerwinski <ikhkolo1@gmail.com>
2018-07-12 17:55:23 +0200

    Comment repository adds likes and dislikes fields to comment object

--- app/Repositories/CommentRepository.php
+++ app/Repositories/CommentRepository.php
@@ -4,7 +4,9 @@ namespace App\Repositories;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Comment;
+use App\Models\CommentRate;
 use App\Models\User;
+use App\Repositories\Rate\CommentRateRepository;
 
 class CommentRepository extends BaseRepository{
 
@@ -13,12 +15,16 @@ class CommentRepository extends BaseRepository{
     }
 
     public function getVideoComments($video_id){
+        $commentRate = new CommentRateRepository(new CommentRate);
+
         $comments =  parent::getWhere([
             'video_id' => $video_id
         ]);
         
         foreach($comments as $comment){
             $comment->creator_name = User::find($comment->user_id)->name;
+            $comment->likes = $commentRate->countLikes($comment->id);
+            $comment->dislikes = $commentRate->countDislikes($comment->id);
         }
         
         return $comments;
